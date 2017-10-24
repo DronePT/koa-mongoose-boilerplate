6ef23bf2c14edea3e6b77003b69d7a4968948e63
André Alves <dronept@gmail.com>
2017-10-24 13:37:32 +0100

    fix to correct code error

--- src/helpers/errors/UnauthorizedError.js
+++ src/helpers/errors/UnauthorizedError.js
@@ -2,7 +2,7 @@ const APIError = require('./APIError')
 
 module.exports = class UnauthorizedError extends APIError  {
   constructor (message = 'Sorry, you are not authorized') {
-    super(message, 401, 1000, true)
+    super(message, 401, 1001, true)
     this.name = this.constructor.name
   }
 }
