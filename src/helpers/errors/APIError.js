9a7e38f4c4867b976add5501a39bf0015132df6f
André Alves <dronept@gmail.com>
2017-10-27 12:06:49 +0100

    add docs to APIerror

--- src/helpers/errors/APIError.js
+++ src/helpers/errors/APIError.js
@@ -1,7 +1,14 @@
 module.exports = class APIError extends Error {
+  /**
+   *
+   * @param {String|Object} message Error message to expose
+   * @param {Number} httpStatus HTTP Status code
+   * @param {Number} code Internal API error code
+   * @param {Boolean} expose expose error to user?
+   */
   constructor (
     message = 'Internal Server Error',
-    status = 500,
+    httpStatus = 500,
     code = 0,
     expose = true
   ) {
@@ -10,7 +17,7 @@ module.exports = class APIError extends Error {
     if (typeof message === 'object') this.data = message
 
     this.name = this.constructor.name
-    this.status = status
+    this.status = httpStatus
     this.expose = expose
     this.code = code
   }
